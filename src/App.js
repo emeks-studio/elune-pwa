f07ab0b7b3cf4bd235f809ccfe0efbb1e2fab53c
educ <39845488+randomeduc@users.noreply.github.com>
2023-04-28 13:52:37 -0300

    Setting up repo
    * Init react app with cra-template-pwa and clean some template code.
    * Adding husky for pre-commit and prettier.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Elune PWA</p>
+      </header>
+    </div>
+  );
+};
+
+export default App;
