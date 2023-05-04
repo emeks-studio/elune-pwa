16f6e11afe643f053325a5ce788f3aceb1c34176
Santiago Monsech <santiago@emeks.com.ar>
2023-05-04 14:44:40 -0300

    migrated index and App js to res

--- src/index.js
+++ src/index.js
@@ -1,15 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
-import "./index.css";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import 'index.bs';
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
